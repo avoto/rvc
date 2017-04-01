d005e3a10d11579ceed8fb9cd3bc262faae7bdd3
Anatoli Radulov <anatoli@radulov.com>
2017-04-01 11:39:35 -0700

    fix build to include babel-preset-es2015-rollup

--- gobblefile.js
+++ gobblefile.js
@@ -2,6 +2,8 @@
 var gobble = require( 'gobble' );
 var resolve = require( 'rollup-plugin-node-resolve' )
 var commonjs = require( 'rollup-plugin-commonjs' )
+var babel = require('rollup-plugin-babel');
+var es2015Rollup = require('babel-preset-es2015-rollup')
 
 module.exports = gobble( 'src' )
 
@@ -14,7 +16,11 @@ module.exports = gobble( 'src' )
 					browser: true,
 					skip:['ractive']
 				}),
-				commonjs()
+				commonjs(),
+                babel({
+                    exclude: 'node_modules/**',
+                    presets: [ es2015Rollup ]
+                })
 			],
 			entry: 'rvc.js',
 			dest: 'rvc.js',
