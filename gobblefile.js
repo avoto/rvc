4c7f72b87232ff43887fe644cc5922f94d79a3eb
Anatoli Radulov <anatoli@estalea.com>
2017-11-20 13:16:25 -0800

    - minification continued

--- gobblefile.js
+++ gobblefile.js
@@ -5,28 +5,32 @@ var commonjs = require( 'rollup-plugin-commonjs' )
 var babel = require('rollup-plugin-babel');
 var es2015Rollup = require('babel-preset-es2015-rollup')
 
-module.exports = gobble( 'src' )
+var nonMinified = gobble( 'src' )
+    .transform( 'rollup', {
+        sourceMap: true,
+        plugins: [
+            resolve({
+                module: true,
+                jsnext: true,
+                main: true,
+                browser: true,
+                skip:['ractive']
+            }),
+            commonjs(),
+            babel({
+                exclude: 'node_modules/**',
+                presets: [ es2015Rollup ]
+            })
+        ],
+        entry: 'rvc.js',
+        dest: 'rvc.js',
+        format: 'amd',
+        moduleName: 'rvc',
+        external: [ 'ractive' ]
+    });
 
-	.transform( 'rollup', {
-            sourceMap: true,
-			plugins: [
-				resolve({
-					module: true,
-					jsnext: true,
-					main: true,
-					browser: true,
-					skip:['ractive']
-				}),
-				commonjs(),
-                babel({
-                    exclude: 'node_modules/**',
-                    presets: [ es2015Rollup ]
-                })
-			],
-			entry: 'rvc.js',
-			dest: 'rvc.js',
-			format: 'amd',
-			moduleName: 'rvc',
-			external: [ 'ractive' ]
-	})
-	.transform('uglifyjs', {ext: '.min.js'});
+module.exports = gobble([
+    nonMinified,
+    nonMinified.transform('uglifyjs', {ext: '.min.js'})
+]);
+// module.exports = nonMinified.transform('uglifyjs', {ext: '.min.js'});
